3d14a0a7eaa7a02c2673f33075c71c4ac64eefe2
Moiseis-Cruz <moizzito@gmail.com>
2023-04-26 19:00:48 -0300

    Programei o js, mas, comentei tudo.

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+// let btnSection = document.querySelectorAll(".btn-section")
+
+// let sectionAtive = document.querySelector(".active")
+
+// let section = document.querySelectorAll("#hero, #sobre-mim, #habilidades, #meus-projetos")
+
+// console.log(section);
+
+// btnSection.forEach((botao) => {
+//     botao.addEventListener("click", () => {
+        
+//         sectionAtive.classList.remove("active")
+
+//         section.classList.add("active")
+//     })
+// })
