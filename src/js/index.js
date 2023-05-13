0469335736d5162f704858f406b24ec207353c2f
Moiseis-Cruz <moizzito@gmail.com>
2023-05-12 21:04:29 -0300

    Começando o .js

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+const btnSection = document.querySelectorAll(".btn-section")
+
+btnSection.forEach(e => {
+    const section = document.querySelectorAll("#sobre-mim, #meus-projetos, #habilidades")
+
+    section
+});
